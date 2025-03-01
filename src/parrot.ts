cc0279e8915cb1d6580f29dc5adece3d387f0ddb
Juan Sánchez <jsmontesinos@gmail.com>
2025-03-01 11:31:42 +0100

    Add static create method

--- src/parrot.ts
+++ src/parrot.ts
@@ -1,50 +1,62 @@
 export enum ParrotTypes {
-    EUROPEAN,
-    AFRICAN,
-    NORWEGIAN_BLUE,
+  EUROPEAN,
+  AFRICAN,
+  NORWEGIAN_BLUE,
 }
 
 export class Parrot {
-    constructor(private parrotType: ParrotTypes,
-                private numberOfCoconuts: number,
-                private voltage: number,
-                private isNailed: boolean) {
-    }
+  constructor(
+    private parrotType: ParrotTypes,
+    private numberOfCoconuts: number,
+    private voltage: number,
+    private isNailed: boolean
+  ) {}
 
-    public getSpeed(): number {
-        switch (this.parrotType) {
-            case ParrotTypes.EUROPEAN:
-                return this.getBaseSpeed();
-            case ParrotTypes.AFRICAN:
-                return Math.max(0, this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts);
-            case ParrotTypes.NORWEGIAN_BLUE:
-                return (this.isNailed) ? 0 : this.getBaseSpeedWithVoltage(this.voltage);
-        }
-        throw new Error("Should be unreachable");
-    }
+  public static create(
+    parrotType: ParrotTypes,
+    numberOfCoconuts: number,
+    voltage: number,
+    isNailed: boolean
+  ): Parrot {
+    return new Parrot(parrotType, numberOfCoconuts, voltage, isNailed);
+  }
 
-    private getBaseSpeed(): number {
-        return 12;
+  public getSpeed(): number {
+    switch (this.parrotType) {
+      case ParrotTypes.EUROPEAN:
+        return this.getBaseSpeed();
+      case ParrotTypes.AFRICAN:
+        return Math.max(
+          0,
+          this.getBaseSpeed() - this.getLoadFactor() * this.numberOfCoconuts
+        );
+      case ParrotTypes.NORWEGIAN_BLUE:
+        return this.isNailed ? 0 : this.getBaseSpeedWithVoltage(this.voltage);
     }
+    throw new Error("Should be unreachable");
+  }
 
-    private getLoadFactor(): number {
-        return 9;
+  private getBaseSpeed(): number {
+    return 12;
+  }
+
+  private getLoadFactor(): number {
+    return 9;
+  }
+
+  private getBaseSpeedWithVoltage(voltage: number): number {
+    return Math.min(24, voltage * this.getBaseSpeed());
+  }
+
+  public getCry(): String {
+    switch (this.parrotType) {
+      case ParrotTypes.EUROPEAN:
+        return "Sqoork!";
+      case ParrotTypes.AFRICAN:
+        return "Sqaark!";
+      case ParrotTypes.NORWEGIAN_BLUE:
+        return this.voltage > 0 ? "Bzzzzzz" : "...";
     }
-
-    private getBaseSpeedWithVoltage(voltage: number): number {
-        return Math.min(24, voltage * this.getBaseSpeed());
-    }
-
-    public getCry(): String {
-        switch (this.parrotType) {
-            case ParrotTypes.EUROPEAN:
-                return "Sqoork!";
-            case ParrotTypes.AFRICAN:
-                return "Sqaark!";
-            case ParrotTypes.NORWEGIAN_BLUE:
-                return this.voltage > 0 ? "Bzzzzzz" : "...";
-        }
-        throw new Error("Should be unreachable");
-    }
-
+    throw new Error("Should be unreachable");
+  }
 }
--- test/parrot.spec.ts
+++ test/parrot.spec.ts
@@ -4,56 +4,56 @@ import assert from "node:assert";
 
 describe("Parrot", () => {
   it("gets speed of European Parrot", () => {
-    const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
+    const parrot = Parrot.create(ParrotTypes.EUROPEAN, 0, 0, false);
     assert.strictEqual(parrot.getSpeed(), 12);
   });
 
   it("gets speed of African Parrot with one coconut", () => {
-    const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
+    const parrot = Parrot.create(ParrotTypes.AFRICAN, 1, 0, false);
     assert.strictEqual(parrot.getSpeed(), 3);
   });
 
   it("gets speed of African Parrot with two coconuts", () => {
-    const parrot = new Parrot(ParrotTypes.AFRICAN, 2, 0, false);
+    const parrot = Parrot.create(ParrotTypes.AFRICAN, 2, 0, false);
     assert.strictEqual(parrot.getSpeed(), 0);
   });
 
   it("gets speed of African Parrot with no coconuts", () => {
-    const parrot = new Parrot(ParrotTypes.AFRICAN, 0, 0, false);
+    const parrot = Parrot.create(ParrotTypes.AFRICAN, 0, 0, false);
     assert.strictEqual(parrot.getSpeed(), 12);
   });
 
   it("gets speed of Norwegian Blue Parrot nailed", () => {
-    const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, true);
+    const parrot = Parrot.create(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, true);
     assert.strictEqual(parrot.getSpeed(), 0);
   });
 
   it("gets speed of Norwegian Blue Parrot not nailed", () => {
-    const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, false);
+    const parrot = Parrot.create(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, false);
     assert.strictEqual(parrot.getSpeed(), 18);
   });
 
   it("gets speed of Norwegian Blue Parrot not nailed high voltage", () => {
-    const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+    const parrot = Parrot.create(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
     assert.strictEqual(parrot.getSpeed(), 24);
   });
 
   it("gets cry of European Parrot", () => {
-    const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
+    const parrot = Parrot.create(ParrotTypes.EUROPEAN, 0, 0, false);
     assert.strictEqual(parrot.getCry(), "Sqoork!");
   });
 
   it("gets cry of African Parrot", () => {
-    const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
+    const parrot = Parrot.create(ParrotTypes.AFRICAN, 1, 0, false);
     assert.strictEqual(parrot.getCry(), "Sqaark!");
   });
   it("gets cry of Norwegian Blue with high voltage", () => {
-    const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+    const parrot = Parrot.create(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
     assert.strictEqual(parrot.getCry(), "Bzzzzzz");
   });
 
   it("gets cry of NorwegianBlue without voltage", () => {
-    const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 0, false);
+    const parrot = Parrot.create(ParrotTypes.NORWEGIAN_BLUE, 0, 0, false);
     assert.strictEqual(parrot.getCry(), "...");
   });
 });
